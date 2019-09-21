0613076fef702597bb23abb5d1b288d2e1391da6
Daniele Fucini <dfucini@gmail.com>
2019-09-21 08:06:54 +0200

    Correct Library.sql
    Now the auto_increment values for Ids start from one as expected

--- Library.sql
+++ Library.sql
@@ -51,7 +51,7 @@ CREATE TABLE `Authors` (
   `DeathYear` int(11) DEFAULT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `Name` (`Name`)
-) ENGINE=InnoDB AUTO_INCREMENT=311 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -83,7 +83,7 @@ CREATE TABLE `Books` (
   CONSTRAINT `Books_ibfk_1` FOREIGN KEY (`Author`) REFERENCES `Authors` (`Id`) ON UPDATE CASCADE,
   CONSTRAINT `Books_ibfk_2` FOREIGN KEY (`Publisher`) REFERENCES `Publishers` (`Id`) ON UPDATE CASCADE,
   CONSTRAINT `Books_ibfk_3` FOREIGN KEY (`Series`) REFERENCES `Series` (`Id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=576 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -98,7 +98,7 @@ CREATE TABLE `Publishers` (
   `Name` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `Name` (`Name`)
-) ENGINE=InnoDB AUTO_INCREMENT=189 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -116,7 +116,7 @@ CREATE TABLE `Series` (
   UNIQUE KEY `Name` (`Name`,`Author`),
   KEY `Author` (`Author`),
   CONSTRAINT `Series_ibfk_1` FOREIGN KEY (`Author`) REFERENCES `Authors` (`Id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -128,4 +128,4 @@ CREATE TABLE `Series` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-09-21  7:54:49
+-- Dump completed on 2019-09-21  7:59:43
